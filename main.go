ccaf63b5c4556a1c5561eabdf926c2c80a83945e
啊这 <87923452+azheea@users.noreply.github.com>
2024-05-03 00:30:55 +0800

    Add files via upload

---
+++ main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func uploadHandler(w http.ResponseWriter, r *http.Request) {
+	// 获取上传的文件
+	file, handler, err := r.FormFile("file")
+	if err != nil {
+		http.Error(w, "Bad Request", http.StatusBadRequest)
+		fmt.Println("Error retrieving file from form:", err)
+		return
+	}
+	defer file.Close()
+
+	// 创建本地文件
+	localFile, err := os.Create("./uploads/" + handler.Filename)
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		fmt.Println("Error creating local file:", err)
+		return
+	}
+	defer localFile.Close()
+
+	// 复制文件内容到本地文件
+	_, err = io.Copy(localFile, file)
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		fmt.Println("Error copying file:", err)
+		return
+	}
+
+	// 返回上传成功的消息和图片访问链接
+	fmt.Fprintf(w, "http://localhost:5000/uploads/%s", handler.Filename)
+}
+
+func imageHandler(w http.ResponseWriter, r *http.Request) {
+	// 获取请求中的图片文件名
+	filename := r.URL.Path[len("/uploads/"):]
+	// 读取图片文件
+	http.ServeFile(w, r, "./uploads/"+filename)
+	fmt.Printf("请求了图片 %s \n", filename)
+}
+
+func main() {
+	// 创建上传目录
+	err := os.MkdirAll("./uploads", os.ModePerm)
+	if err != nil {
+		fmt.Println("Error creating uploads directory:", err)
+		return
+	}
+
+	// 设置路由
+	http.HandleFunc("/upload", uploadHandler)
+	http.HandleFunc("/uploads/", imageHandler)
+
+	// 启动服务器
+	fmt.Println("Local image server started on http://localhost:5000")
+	http.ListenAndServe(":5000", nil)
+}
